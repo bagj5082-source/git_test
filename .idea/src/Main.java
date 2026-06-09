af980d9e081f705e2f1ab95fb73632dd5dac3289
JIMIN <bagj5082@gmail.com>
2026-06-09 11:26:26 +0900

    테스트테스트

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        int a = 4;
+        int b = 3;
+        int c = a + b;
+        System.out.println("c = " + c);
 
     }
 }
